22a1931a5f7d6bc8c656f14095434eb4838bbd99
JackChan829 <liang.chen829@gmail.com>
2019-02-16 22:48:25 +0800

    添加 用两个栈实现队列

---
+++ 《剑指Offer》/用两个栈实现队列.swift
@@ -0,0 +1,44 @@
+/**
+	面试题 9: 用两个栈实现队列
+**/
+
+struct Stack<Element>{
+	var items = [Element]()
+	mutating func push(_ item : Element){
+		items.append(item)
+	}
+	mutating func pop() -> Element? {
+		if items.count > 0 {
+			return items.removeLast()
+		}else{
+			return nil
+		}
+	}
+	func isEmpty() -> Bool {
+		if items.count > 0 {
+			return false
+		}else{
+			return true
+		}
+	}
+}
+
+struct Queue <Element>{
+	var stackA = Stack<Element>()
+	var stackB = Stack<Element>()
+	mutating func appendTail(_ item:Element){
+		while !stackB.isEmpty() {
+			let itemB = stackB.pop()
+			stackA.push(itemB!)
+		}
+		stackA.push(item)
+	}
+	
+	mutating func deleteHead() -> Element?{
+		while !stackA.isEmpty() {
+			let item = stackA.pop()
+			stackB.push(item!)
+		}
+		return stackB.pop()
+	}
+}
