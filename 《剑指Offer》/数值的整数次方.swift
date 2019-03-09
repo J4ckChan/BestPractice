5c40848007abc6bbde4b54e52695eb5639d087f5
JackChan829 <liang.chen829@gmail.com>
2019-03-09 22:06:03 +0800

    修改 数值的整数次方

--- 《剑指Offer》/数值的整数次方.swift
+++ 《剑指Offer》/数值的整数次方.swift
@@ -4,6 +4,8 @@
 
 var invalidInput = false
 
+var invalidInput = false
+
 func power(_ base:Double,_ exponent:Int) -> Double {
 	invalidInput = false
 	
@@ -11,31 +13,33 @@ func power(_ base:Double,_ exponent:Int) -> Double {
 		invalidInput = true
 		return 0.0
 	}
-	
-	if exponent == 0 {
-		return 1
-	}else if exponent == 1 {
-		return base
-	}
-	
+		
 	var absExponent = exponent
 	if exponent < 0 {
 		absExponent = -exponent
 	}
 	
-	var res = 1.0
-	let halfOfAbsExponent = absExponent >> 1
-	for _ in 0..<halfOfAbsExponent{
-		res*=base
-	}
-	res*=res
-	if (absExponent & 0x1) == 1 {
-		res*=base
-	}
+	var res = powerWithUnsignedExponent(base,absExponent)
 	
 	if exponent < 0 {
 		res = 1.0/res
 	}
 	
+	return res
+}
+
+func powerWithUnsignedExponent(_ base:Double, _ exponent:Int) -> Double{
+	if exponent == 0 {
+		return 1
+	}
+	if exponent == 1 {
+		return base
+	}
+	
+	var res = powerWithUnsignedExponent(base,exponent>>1)
+	res*=res
+	if (exponent & 0x1) == 1 {
+		res*=base
+	}
 	return res
 }
