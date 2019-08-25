c997db77ec254a4027bd04cc4d2b4904ad0895eb
JackChan829 <liang.chen829@gmail.com>
2019-08-25 14:55:30 +0800

    把数字翻译成字符串：递归 → 循环

--- 《剑指Offer》/把数字翻译成字符串.swift
+++ 《剑指Offer》/把数字翻译成字符串.swift
@@ -3,43 +3,47 @@
 /// - Parameter number: 输入一个数字
 /// - Returns: 翻译方法的个数
 func getTranslationCount(_ number:Int) -> Int {
-	
-	func getTranslationCount(_ number:String,_ count:inout Int){
-		
-		guard !number.isEmpty else {
-			return
-		}
-		
-		var numStr = number
-		
-		if numStr.count == 1 {
-			count += 1
-			return
-		}else if numStr.count == 2 {
-			if let num = Int(numStr) {
-				if num >= 0 && num < 26 {
-					count += 1
+
+	func getTranslationCount(_ number:String) -> Int {
+
+		var counts = Array.init(repeating: 0, count: number.count)
+		var count = 0
+		let length = number.count
+
+		for i in (0..<length).reversed() {
+			count = 0
+			if i < length-1 {
+				count = counts[i+1]
+			}else{
+				count = 1
+			}
+
+			if i < length-1 {
+				let endIndex = number.index(number.startIndex, offsetBy: i+1)
+				let startIndex = number.index(number.startIndex, offsetBy: i)
+				let numStr = String(number[startIndex...endIndex])
+				if let num = Int(numStr) {
+					if num >= 10 && num <= 25 {
+						if i < length - 2 {
+							count += counts[i+2]
+						}else{
+							count += 1
+						}
+					}
 				}
 			}
+			counts[i] = count
 		}
-		
-		let first = String(numStr.removeFirst())
-		let second = String(numStr.removeFirst())
-		
-		getTranslationCount(second+numStr, &count)
-		
-		let firstTwo = first + second
-		if let firstTwoNum = Int(firstTwo) {
-			if firstTwoNum >= 0 && firstTwoNum < 26 {
-				getTranslationCount(numStr, &count)
-			}
-		}
+		count = counts[0]
+
+		return count
 	}
 	
-	var count = 0
-	getTranslationCount(String(number), &count)
-	
-	return count
+	guard number >= 0 else {
+		return 0
+	}
+
+	return getTranslationCount(String(number))
 }
 
 let res = getTranslationCount(12258)
